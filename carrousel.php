8b37f8d2ba73426336f77c4a9d83d78723d26b1e
Farzad <farzad@Farzads-MacBook-Air.local>
2023-03-08 10:51:36 -0500

    c11 debut du carousel

---
+++ carrousel.php
@@ -0,0 +1,13 @@
+<?php
+/** 
+ * Extension carrousel permet d'afficher dans boite modale
+ * les image d'une galerie
+ * Package name : Carrousel
+ * Version : 1.0.0
+*/
+/*
+Plugin Name: Carrousel
+Author : Eddy Martin
+Plugin URI: https:github.com/eddytuto/carrouse
+Description : Permet d'afficher dans une boite modale les images d'une galerie
+*/
