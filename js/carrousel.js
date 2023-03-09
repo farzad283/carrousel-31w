eef4d233d470431003ea41394a864df12d874eaf
Farzad <farzad@Farzads-MacBook-Air.local>
2023-03-09 15:54:35 -0500

    c12 affichage et animation de la boite modale

--- js/carrousel.js
+++ js/carrousel.js
@@ -2,5 +2,34 @@
 (function(){
     // fonction IFEE
     console.log('début du carrousel')
+    let bouton__ouvrir = document.querySelector('.bouton__ouvrir')
+    let elmCarrousel = document.querySelector('.carrousel')
+    let elmBouton__x = document.querySelector('.bouton__x')
+    let elmGalerie = document.querySelector('.galerie')
+    let elmGalerie__img = elmGalerie.querySelectorAll('img')
+    let elmGalerie__figure = document.querySelector('.galerie__figure')
+    console.log(elmGalerie__img.length)
+
+
+
+    for (const elmImg of elmGalerie__img){
+        let elmCarrousel__img = document.createElement('img')
+        elmCarrousel__img.setAttribute('src', elmImg.getAttribute('src'))
+        elmGalerie__figure.appendChild(elmCarrousel__img)
+
+    }
+
+
+
+
+    console.log(bouton__ouvrir.tagName)
     
+    bouton__ouvrir.addEventListener('mousedown', function(){
+        console.log('boîte modale')
+        elmCarrousel.classList.add('carrousel--ouvrir')
+    })
+    elmBouton__x.addEventListener('mousedown', function(){
+        console.log('boîte modale')
+        elmCarrousel.classList.remove('carrousel--ouvrir')
+    })
     })()
